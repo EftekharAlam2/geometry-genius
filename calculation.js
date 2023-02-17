3a667cfcb5795913f592a628cf7de39d2c036bb5
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-02-18 01:56:10 +0600

    5th

---
+++ calculation.js
@@ -0,0 +1,30 @@
+function inputField(id1) {
+  const value1 = document.getElementById(id1);
+  const valueString = value1.value;
+  const finalValue = parseFloat(valueString);
+  value1.value = "";
+  if (isNaN(finalValue)) {
+    return alert("Please Enter the correct value");
+  } else if (finalValue.toString().length != valueString.length) {
+    return alert("Please Enter the correct value");
+  } else {
+    if (finalValue < 0) {
+      return alert("Please Enter the posu correct value");
+    } else {
+      return finalValue;
+    }
+  }
+}
+
+document
+  .getElementById("triangle-button")
+  .addEventListener("click", function () {
+    const firstValue = inputField("triangle-input-01");
+    const secondValue = inputField("triangle-input-02");
+    const triangleResult = 0.5 * firstValue * secondValue;
+    if (isNaN(triangleResult)) {
+      return;
+    } else {
+      console.log(triangleResult.toFixed(2));
+    }
+  });
